fa49eb9bcf2c82009834be699692cdedb95e21d7
Matt Hixon <mhixon@cougar.cs.ksu.edu>
2017-03-05 15:13:56 -0600

    worked on user access memory

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -88,6 +88,10 @@ start_process (void *file_name_)
 int
 process_wait (tid_t child_tid UNUSED) 
 {
+  while(true)
+  {
+    
+  }
   return -1;
 }
 
@@ -437,7 +441,7 @@ setup_stack (void **esp)
     {
       success = install_page (((uint8_t *) PHYS_BASE) - PGSIZE, kpage, true);
       if (success)
-        *esp = PHYS_BASE;
+        *esp = PHYS_BASE - 12;
       else
         palloc_free_page (kpage);
     }
--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -3,6 +3,10 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
+#include "userprog/pagedir.h"
+
+static uint32_t *active_pd (void);
 
 static void syscall_handler (struct intr_frame *);
 
@@ -15,6 +19,94 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
+
+	if(is_kernel_vaddr(f->esp) && f->esp == NULL && !pagedir_get_page(active_pd(), f->esp))
+	{
+		// Terminate and Free Resources
+	} 
+	else
+	{
+		switch((int) f->esp)
+		{
+			case SYS_HALT:
+
+				break;
+
+			case SYS_EXIT:
+
+				break;
+			
+			case SYS_EXEC:
+
+				break;
+			
+			case SYS_WAIT:
+
+				break;
+			
+			case SYS_CREATE:
+
+				break;
+			
+			case SYS_REMOVE:
+
+				break;
+			
+			case SYS_OPEN:
+
+				break;
+			
+			case SYS_FILESIZE:
+
+				break;
+			
+			case SYS_READ:
+
+				break;
+			
+			case SYS_WRITE:
+
+				break;
+			
+			case SYS_SEEK:
+
+				break;
+			
+			case SYS_TELL:
+
+				break;
+			
+			case SYS_CLOSE:
+
+				break;
+
+			default:
+				break;
+		}
+	}
+
   printf ("system call!\n");
   thread_exit ();
 }
+
+
+
+
+
+
+
+
+void halt (void) {}
+void exit (int status) {}
+pid_t exec (const char *file) {return 0;}
+int wait (pid_t pid) {return 0;}
+bool create (const char *file, unsigned initial_size) {return true;}
+bool remove (const char *file) {return true;}
+int open (const char *file) {return 0;}
+int filesize (int fd) {return 0;}
+int read (int fd, void *buffer, unsigned length) {return 0;}
+int write (int fd, const void *buffer, unsigned length) {return 0;}
+void seek (int fd, unsigned position) {}
+unsigned tell (int fd) {return 0;}
+void close (int fd) {}
+
